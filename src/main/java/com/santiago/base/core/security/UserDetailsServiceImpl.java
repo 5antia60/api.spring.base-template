57d3ab3471eabafb40ce008a6b292236f5549580
5antia60 <santiago.delgado2001@outlook.com>
2026-02-19 15:59:06 -0300

    feature(security): added security classes and methods for Bearer JWT token

---
+++ src/main/java/com/santiago/base/core/security/UserDetailsServiceImpl.java
@@ -0,0 +1,21 @@
+package com.santiago.base.core.security;
+
+import com.santiago.base.modules.users.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado: " + email));
+    }
+}
