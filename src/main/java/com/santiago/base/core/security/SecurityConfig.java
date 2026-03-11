647298b7121434939922eb2470773794de3b3425
5antia60 <sdelgado3@lenovo.com>
2026-03-11 15:42:26 -0300

    refactor(SecurityConfig): removed unused component

--- src/main/java/com/santiago/base/core/security/SecurityConfig.java
+++ src/main/java/com/santiago/base/core/security/SecurityConfig.java
@@ -57,7 +57,6 @@ public class SecurityConfig {
                         .requestMatchers("/api/auth/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/users").hasRole("ADMIN")
                         .requestMatchers(HttpMethod.DELETE, "/api/users/**").hasRole("ADMIN")
-//                        .requestMatchers("/api/tasks/**").authenticated()
                         .anyRequest().authenticated()
                 )
                 .sessionManagement(session -> session
