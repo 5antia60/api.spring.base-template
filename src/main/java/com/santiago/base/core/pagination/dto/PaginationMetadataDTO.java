5aafa8d8ef79d3fc95b4905171a1d68ed699eb03
5antia60 <sdelgado3@lenovo.com>
2026-03-16 13:18:41 -0300

    feature(PaginationMetadataDTO): added validations on constructor initialization

--- src/main/java/com/santiago/base/core/pagination/dto/PaginationMetadataDTO.java
+++ src/main/java/com/santiago/base/core/pagination/dto/PaginationMetadataDTO.java
@@ -6,4 +6,21 @@ public record PaginationMetadataDTO(
         int totalPages,
         long totalElements
 ) {
+    public PaginationMetadataDTO {
+        if (pageSize < 0) {
+            throw new IllegalArgumentException("pageSize deve ser maior ou igual a 0");
+        }
+
+        if (pageNumber < 0) {
+            throw new IllegalArgumentException("pageNumber deve ser maior ou igual a 0");
+        }
+
+        if (totalPages < 0) {
+            throw new IllegalArgumentException("totalPages deve ser maior ou igual a 0");
+        }
+
+        if (totalElements < 0) {
+            throw new IllegalArgumentException("totalElements deve ser maior ou igual a 0");
+        }
+    }
 }
